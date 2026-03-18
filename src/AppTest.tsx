29960b3cc22dc6597ec7adc79784a88cbe81ac18
Brenno Morais <brennomoraisdev@gmail.com>
2026-03-17 20:32:10 -0400

    chore: remover arquivos de teste temporários

--- src/AppTest.tsx
+++
@@ -1,8 +0,0 @@
-export default function AppTest() {
-  return (
-    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>✅ React está funcionando!</h1>
-      <p>Se você vê esta mensagem, o React foi renderizado com sucesso.</p>
-    </div>
-  );
-}
